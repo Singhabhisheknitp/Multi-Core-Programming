a12a0afae6f62c0090a352a76439beb082b77289
Abhishek Kumar <Singhabhisheknitp@gmail.com>
2024-03-11 18:39:26 +0530

    added plot.py for visualizing the performance curve

---
+++ plot.py
@@ -0,0 +1,20 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+
+df = pd.read_csv('locklatency.csv', skiprows=1, names=['T', 'Time_TAS', 'Time_TTAS', 'Time_Backoff'])
+
+# Plot with specified line width and marker style
+ax = df.plot(x='T', y=['Time_TAS', 'Time_TTAS', 'Time_Backoff'], kind='line', linewidth=3, 
+             style=['o-', 'x-', '*-'], markersize=8)
+
+plt.title('Threadnum vs Time', fontsize=7)
+plt.xlabel('Threadnum', fontsize=7)
+plt.ylabel('Time', fontsize=7)
+
+# Adjust legend
+ax.legend(["Time_TAS", "Time_TTAS", "Time_Backoff"], fontsize=7)
+
+plt.grid(True)
+plt.savefig('plot.png')
+
+
