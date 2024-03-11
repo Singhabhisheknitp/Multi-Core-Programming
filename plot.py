5b75cb868e8a3f4ea131b9809cf5ab8dfad85a8e
Abhishek Kumar <Singhabhisheknitp@gmail.com>
2024-03-11 19:22:32 +0530

    adjusted plot.py after backoff removal

--- plot.py
+++ plot.py
@@ -1,10 +1,10 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 
-df = pd.read_csv('locklatency.csv', skiprows=1, names=['T', 'Time_TAS', 'Time_TTAS', 'Time_Backoff'])
+df = pd.read_csv('locklatency.csv', skiprows=1, names=['T', 'Time_TAS', 'Time_TTAS'])
 
 # Plot with specified line width and marker style
-ax = df.plot(x='T', y=['Time_TAS', 'Time_TTAS', 'Time_Backoff'], kind='line', linewidth=3, 
+ax = df.plot(x='T', y=['Time_TAS', 'Time_TTAS'], kind='line', linewidth=3, 
              style=['o-', 'x-', '*-'], markersize=8)
 
 plt.title('Threadnum vs Time', fontsize=7)
@@ -12,7 +12,7 @@ plt.xlabel('Threadnum', fontsize=7)
 plt.ylabel('Time', fontsize=7)
 
 # Adjust legend
-ax.legend(["Time_TAS", "Time_TTAS", "Time_Backoff"], fontsize=7)
+ax.legend(["Time_TAS", "Time_TTAS"], fontsize=7)
 
 plt.grid(True)
 plt.savefig('plot.png')
