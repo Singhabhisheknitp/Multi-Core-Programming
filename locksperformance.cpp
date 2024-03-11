c0be4d4dc7804527b6286ffb789c13e918f5e91b
Abhishek Kumar <Singhabhisheknitp@gmail.com>
2024-03-11 18:17:25 +0530

    deleted backoff

--- locksperformance.cpp
+++ locksperformance.cpp
@@ -77,57 +77,12 @@ void cs_TTAS(){
 }
 
 
-
-class BackoffLock {
-public:
-    atomic<bool> state;
-    int min_delay;
-    int max_delay;
-    BackoffLock(int min, int max) : state(false), min_delay(min), max_delay(max) {}
-    
-
-    void lock() {
-        while (true) {
-            while (this->state.load()) {}
-            if (!this->state.exchange(true)) {
-                return;
-            } else {
-                int delay = rand() % min_delay;
-                usleep(delay);
-                delay = min(2*delay, max_delay);        
-        }
-    }}
-    void unlock(){
-        (this->state).store(false);
-    }
-
-};
-BackoffLock* o = new BackoffLock(1, 5);
-void cs_Backoff(){
-    for (int j = 0; j < 100000; j++){
-        o->lock();
-        //CS 
-    int c = 0; int d = 0; int e = 0; int f = 0;
-    for (int i = 0; i < 100; i++){
-        c = c+1; 
-        d = d+1;
-        e = e+1;
-        f = f+1;
-    }  
-        o->unlock();
-       
-    }
-}
-
-
-
-
 int main(){
     
     
     ofstream locklatency("locklatency.csv");
     // locklatency<<"T,Time_TAS, Time_TTAS\n";
-    locklatency<<"T, Time_TAS, Time_Backoff\n";
+    locklatency<<"T, Time_TAS, Time_TTAS\n";
 
 
     int threadnum =1;
@@ -148,17 +103,7 @@ int main(){
     chrono::duration<double> elapsed1 = end_TTAS - start_TTAS;
     double time_TTAS = elapsed1.count()/threadnum;
 
-    threads.clear();
-    auto start_Backoff = chrono::high_resolution_clock::now();
-    for(int i = 1; i <= threadnum; i++) { threads.push_back(thread(cs_Backoff));}
-    for(auto& thread : threads) {thread.join();}
-    auto end_Backoff = chrono::high_resolution_clock::now();
-    chrono::duration<double> elapsed2 = end_Backoff - start_Backoff;
-    double time_Backoff = elapsed2.count()/threadnum;
-
-
-
-    locklatency<<threadnum<<","<<time_TAS<<","<<time_TTAS<<","<<time_Backoff<<"\n";
+    locklatency<<threadnum<<","<<time_TAS<<","<<time_TTAS<<","<<"\n";
     threadnum = threadnum + 4;
    
 }
