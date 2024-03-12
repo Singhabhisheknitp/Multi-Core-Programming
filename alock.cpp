a35a2db76f03c01527b10ff2c6871ccf41bad413
Abhishek Kumar <Singhabhisheknitp@gmail.com>
2024-03-12 18:50:34 +0530

    made plot.py configurable as per .csv file

--- alock.cpp
+++ alock.cpp
@@ -8,11 +8,41 @@
 #include <unistd.h>
 using namespace std;
 
+class ALock {
+public:
+ // id variable local to each thread but global across the object as needed
+   static thread_local atomic<int> id;
+   atomic<int>* tail;
+    bool* flag;
+int size;
+ALock(int numthread) {
+    size = numthread;
+    id.store(0);
+    tail = new atomic<int>(0);
+    flag = new bool[size]();
+    flag[0] = true;
+}
 
+void lock() {
+    int slot = tail->fetch_add(1) % size;
+    id.store(slot);
+    while (!flag[slot]) {}
+}
 
-void cs_alock(){
+    void unlock() {
+        int slot = id.load();
+        flag[slot] = false;
+        flag[(slot + 1) % size] = true;}
+
+       
+    };
+thread_local atomic<int> ALock::id(0);
+
+void cs_alock(int numthread){
+    ALock* p = new ALock(numthread);
+    
     for (int j = 0; j < 100000; j++){
-        n->lock();
+        p->lock();
         //CS 
     int c = 0; int d = 0; int e = 0; int f = 0;
     for (int i = 0; i < 100; i++){
@@ -21,7 +51,28 @@ void cs_alock(){
         e = e+1;
         f = f+1;
     }  
-        n->unlock();
+        p->unlock();
        
     }
+}
+
+int main(){
+    ofstream locklatency("locklatency.csv");
+    // locklatency<<"T,Time_alock\n";
+    locklatency<<"T, Time_alock\n";
+    int threadnum = 1;
+    while (  threadnum <= 40){
+    vector<thread> threads;
+    auto start_alock = chrono::high_resolution_clock::now(); 
+    for(int i = 1; i <= threadnum; i++) { threads.push_back(thread(cs_alock, threadnum));}
+    for(auto& thread : threads) {thread.join();}
+    auto end_alock = chrono::high_resolution_clock::now();
+    chrono::duration<double> elapsed = end_alock - start_alock;
+    double time_alock = elapsed.count()/threadnum;
+
+    locklatency<<threadnum<<","<<time_alock<<"\n";
+    threadnum = threadnum + 4;
+}
+locklatency.close();
+return 0;
 }
--- plot.py
+++ plot.py
@@ -1,19 +1,15 @@
 import pandas as pd
 import matplotlib.pyplot as plt
-
-df = pd.read_csv('locklatency.csv', skiprows=1, names=['T', 'Time_TAS', 'Time_TTAS'])
-
-# Plot with specified line width and marker style
-ax = df.plot(x='T', y=['Time_TAS', 'Time_TTAS'], kind='line', linewidth=3, 
-             style=['o-', 'x-'], markersize=8)
+df = pd.read_csv('locklatency.csv')
+columns = df.columns.tolist()
+columns_num = len(columns) - 1
+styles = ['*-', 'o-', 'x-', '+-']
+ax = df.plot(x=columns[0], y=columns[1:], kind='line', linewidth=3, style=styles[:columns_num], markersize=8)
 
 plt.title('Threadnum vs Time', fontsize=7)
 plt.xlabel('Threadnum', fontsize=7)
 plt.ylabel('Time', fontsize=7)
-
-# Adjust legend
-ax.legend(["Time_TAS", "Time_TTAS"], fontsize=7)
-
+ax.legend(df.columns[1: ], fontsize=7)
 plt.grid(True)
 plt.savefig('plot.png')
 
